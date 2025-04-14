2706a0cad4d29265bfd2a164f002a279020690ac
mehu-manya <157779296+mehu-manya@users.noreply.github.com>
2025-04-14 16:39:51 +0530

    Initial commit

---
+++ app/page.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@stackframe/stack";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Subscribe</h2>
+    
+    <Button variant={'destructive'}>Subscribe</Button>
+    <UserButton></UserButton>
+    </div>
+    
+  );
+}
